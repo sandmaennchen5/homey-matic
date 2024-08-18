fb48c7bc501a368de15dee46678237d28885630a
Robin Baum <robinbaum@web.de>
2024-08-18 13:56:45 +0200

    Refactor async initialization, error handling, and resource management

--- app.js
+++ app.js
@@ -2,11 +2,11 @@
 
 const Homey = require('homey');
 const HomeMaticDiscovery = require('./lib/HomeMaticDiscovery');
-const HomeMaticCCUJack = require('./lib/HomeMaticCCUJack'); // Behalten Sie nur diesen Import
+const HomeMaticCCUJack = require('./lib/HomeMaticCCUJack'); 
 const Constants = require('./lib/constants');
 const Logger = require('./lib/logger');
 
-const connTypeCCUJack = 'use_ccu_jack'; // Nur dieser Verbindungstyp bleibt bestehen
+const connTypeCCUJack = 'use_ccu_jack';
 
 class Homematic extends Homey.App {
     async onInit() {
@@ -23,7 +23,7 @@ class Homematic extends Homey.App {
             const storedBridges = this.getStoredBridges();
             if (Object.keys(storedBridges).length > 0) {
                 this.logger.log('info', 'Initializing stored bridges...');
-                this.initializeStoredBridges(storedBridges);
+                await this.initializeStoredBridges(storedBridges);
             } else {
                 await this.discovery.discover();
             }
@@ -35,13 +35,8 @@ class Homematic extends Homey.App {
     async onUninit() {
         this.logger.log('info', 'Unloading homematic app...');
 
-        const cleanupPromises = Object.keys(this.bridges).map(serial => {
-            if (this.bridges[serial].cleanup) {
-                return this.bridges[serial].cleanup();
-            }
-        });
-
-        // Wait for all cleanup operations to finish
+        const cleanupPromises = Object.values(this.bridges).map(bridge => bridge.cleanup?.());
+        
         await Promise.all(cleanupPromises);
 
         this.logger.log('info', 'All bridges cleaned up.');
@@ -60,31 +55,31 @@ class Homematic extends Homey.App {
         const bridges = {};
         this.homey.settings.getKeys().forEach((key) => {
             if (key.startsWith(Constants.SETTINGS_PREFIX_BRIDGE)) {
-                let bridge = this.homey.settings.get(key);
+                const bridge = this.homey.settings.get(key);
                 bridges[bridge.serial] = bridge;
             }
         });
         return bridges;
     }
 
-    initializeStoredBridges(bridges) {
-        Object.keys(bridges).forEach((serial) => {
-            let bridge = bridges[serial];
+    async initializeStoredBridges(bridges) {
+        for (const serial of Object.keys(bridges)) {
+            const bridge = bridges[serial];
             this.logger.log('info', "Initializing stored CCU:", "Type", bridge.type, "Serial", bridge.serial, "IP", bridge.address);
-            this.initializeBridge(bridge);
-        });
+            await this.initializeBridge(bridge);
+        }
     }
 
     getConnectionType() {
         return connTypeCCUJack;
     }
 
-    initializeBridge(bridge) {
+    async initializeBridge(bridge) {
         try {
             const connType = this.getConnectionType();
             this.logger.log('info', 'Connection type:', connType);
             this.logger.log('info', "Initializing CCU Jack");
-            this.bridges[bridge.serial] = new HomeMaticCCUJack(
+            const bridgeInstance = new HomeMaticCCUJack(
                 this.logger,
                 this.homey,
                 bridge.type,
@@ -94,15 +89,17 @@ class Homematic extends Homey.App {
                 this.settings.ccu_jack_user,
                 this.settings.ccu_jack_password,
             );
-            return this.bridges[bridge.serial];
+            this.bridges[bridge.serial] = bridgeInstance;
+            return bridgeInstance;
         } catch (err) {
             this.logger.log('error', `Failed to initialize bridge ${bridge.serial}:`, err);
         }
     }
 
     setBridgeAddress(serial, address) {
-        if (this.bridges[serial]) {
-            this.bridges[serial].address = address;
+        const bridge = this.bridges[serial];
+        if (bridge) {
+            bridge.address = address;
         } else {
             this.logger.log('error', `No bridge found for serial: ${serial}`);
         }
@@ -110,7 +107,7 @@ class Homematic extends Homey.App {
 
     deleteStoredBridges() {
         const bridges = this.getStoredBridges();
-        Object.keys(bridges).forEach((serial) => {
+        bridges.forEach((serial) => {
             this.homey.settings.unset(Constants.SETTINGS_PREFIX_BRIDGE + serial);
         });
     }
